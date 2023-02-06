3ca48b2ee8a8ea06bc2e4d98aae438c2f0840378
NohEunSeok <eodyddnjs@naver.com>
2023-02-06 21:23:49 +0900

    * 인덱스 컨트롤러 에러 수정

--- src/main/java/com/nos/interview/controller/IndexController.java
+++ src/main/java/com/nos/interview/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.nos.interview.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,7 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class IndexController {
 
     @GetMapping("/index")
-    public void index(String name, ModelAndView modelAndView){
-        modelAndView.addObject("name", name);
+    public void index(String name, Model modelAndView){
+        modelAndView.addAttribute("name", name);
     }
 }
